d6b067501163c65c1a3db847611ce68605c73794
dukelester <dukelester4@gmail.com>
2022-10-10 13:58:26 +0300

    The OOP Concept
    we saw how to apply inheritance and multiple inheritances to classes and apply
    polymorphism to methods. We then learned how to create abstract classes and methods. Finally, we
    learned the concept of encapsulation and how to restrict access to methods and variables of a class.

--- metaprogramming/OOP/encapsulation.py
+++ metaprogramming/OOP/encapsulation.py
@@ -10,7 +10,7 @@ name, brand, purchase price, and profit margin and create a private method to di
 details. """
     branch_id = 98745678
     reagonal_manager = "Duke lester"
-    branch_mamnager = "John Kimbo"
+    branch_manager = "John Kimbo"
 
     __product_id = 987655672
     __product_name = "Addidas Shoes Green"
@@ -38,3 +38,53 @@ details. """
 test_branch = Branch()
 print(test_branch.branch_id)
 # print(test_branch._Branch__display_product_details())
+
+
+class BranchRecreated:
+    """ Recreate the Branch class with protected variables for product ID, product
+    name, brand, purchase price, and profit margin and create a protected method to display
+    the product details. We will create a branch manager as a private variable.
+    We will also create branch ID and regional manager as class variables that are
+    not protected or private and look at the difference in accessing those using objects
+    outside the class. We will also inherit the Branch class further to check which members are accessible.
+"""
+    branch_id = 98745678
+    reagonal_manager = "Duke lester"
+    branch_manager = "John Kimbo"
+
+    _product_id = 987655672
+    _product_name = "Addidas Shoes Green"
+    _purchase_price = 4000
+    _product_brand = None
+    _produc_profit_margin = None
+    __branch_manager_1 = "Lester dlester"
+
+    def _display_product_details(self):
+        """ Private method to display the product details """
+        self._product_id = 34567890
+        self._product_name = "Johnsons Soccer Boots"
+        self._purchase_price = 8000
+        self._product_brand = "The Johnsons"
+        self._produc_profit_margin = 0.67
+        return f" Product Id : {self._product_id} Product name : {self._product_name} \
+                Product Purchase Price : {self._purchase_price} Product Brand : {self._product_brand} \
+                Product profit margin : {self._produc_profit_margin}, Branch Manager{self.__branch_manager_1}"
+
+    def __init__(self):
+        self._display_product_details()
+branch = BranchRecreated()
+print(branch._display_product_details(), " ==>Protected member")
+
+class Nairobi(BranchRecreated):
+    """ inherits the parent class, Branch. The child class
+will inherit all the protected variables and methods from the parent class,
+whereas it will still not inherit the private members:
+"""
+    def __init__(self):
+        super().__init__()
+        print(self._product_id, "The protected ")
+        print(self._display_product_details())
+
+new_nairobi = Nairobi()
+print(new_nairobi, new_nairobi.branch_manager)
+print(new_nairobi.__branch_manager_1) # AttributeError
